e3d2a907dca2faca40049b114092fca6829b9e9f
卢旭鸣 <luxuming@xmfunny.com>
2024-06-07 20:08:53 +0800

    <feature> first

---
+++ ExchangeGen/GenerateTool.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ExchangeGen {
+
+    public interface MetaProperty {
+        Type PropertyType { get; }
+        string PropertyName { get; }
+    }
+
+    public enum ParameterModifyType {
+        None = 1,
+        In = 2,
+        Out = 3,
+        Ref = 4,
+    }
+    
+    public interface MetaMethodParameter {
+        Type PropertyType { get; }
+        string PropertyName { get; }
+        ParameterModifyType ModifyType { get; }
+    }
+    
+    public interface MetaMethod {
+        SyntaxKind MethodModify { get; }
+        Type RetType { get; }
+        string MethodName { get; }
+        MetaMethodParameter[] Paramters { get; }
+    }
+    
+    public interface MetaClass {
+        string ClassName { get; }
+        string Namespcae { get; }
+        MetaProperty[] Property { get; }
+    }
+    
+    public class LuaStackTool {
+
+        public string CreateClass(MetaClass metaClass) {
+            
+            // 创建类
+            ClassDeclarationSyntax classDeclarationSyntax = CreateClassDeclaration(metaClass);
+            
+            // 创建名空间
+            NamespaceDeclarationSyntax namespaceDeclarationSyntax = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName(metaClass.Namespcae))
+                .AddMembers(classDeclarationSyntax);
+
+            // 创建编译单元
+            CompilationUnitSyntax compilationUnitSyntax = SyntaxFactory.CompilationUnit()
+                .AddMembers(namespaceDeclarationSyntax);
+
+            // 字符串代码
+            string formattedCode = FormatCode(compilationUnitSyntax);
+            
+            // 代码写入文件
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), $"{metaClass.ClassName}.cs");
+            File.WriteAllText(filePath, formattedCode);
+        }
+        string FormatCode(CompilationUnitSyntax compilationUnitSyntax) {
+            StringBuilder builder = new StringBuilder();
+            using (StringWriter writer = new StringWriter(builder)) {
+                compilationUnitSyntax.WriteTo(writer);
+            }
+            return builder.ToString();
+        }
+
+        ClassDeclarationSyntax CreateClassDeclaration(MetaClass metaClass) {
+            // 类声明
+            ClassDeclarationSyntax classDeclarationSyntax = SyntaxFactory.ClassDeclaration(metaClass.ClassName).
+                AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            foreach (MetaProperty metaProperty in metaClass.Property) {
+                PropertyDeclarationSyntax propertyDeclarationSyntax = SyntaxFactory.PropertyDeclaration(SyntaxFactory.IdentifierName(metaProperty.PropertyType.Name), SyntaxFactory.Identifier(metaProperty.PropertyName))
+                    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                    .AddAccessorListAccessors(
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                            .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
+                        SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                            .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
+
+                classDeclarationSyntax.AddMembers(propertyDeclarationSyntax);
+
+            }
+            
+        }
+    }
+}
---
+++ ExchangeGenConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExchangeGenConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExchangeGenConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5FFAFC8F-5700-4500-B24E-A145144C8078")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
